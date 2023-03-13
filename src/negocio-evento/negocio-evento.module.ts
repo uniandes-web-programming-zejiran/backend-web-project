530442f7d0bdc76ff1ab9f6569697662775b4989
CamiloFallaM <c.falla@uniandes.edu.co>
2023-03-12 22:24:24 -0500

    NegocioEventoController

--- src/negocio-evento/negocio-evento.module.ts
+++ src/negocio-evento/negocio-evento.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { NegocioEntity } from 'src/negocio/negocio.entity';
 import { EventoEntity } from 'src/evento/evento.entity';
 import { NegocioEventoService } from './negocio-evento.service';
+import { NegocioEventoController } from './negocio-evento.controller';
 
 @Module({
   providers: [NegocioEventoService],
   imports: [TypeOrmModule.forFeature([NegocioEntity, EventoEntity])],
+  controllers: [NegocioEventoController],
 })
 export class NegocioEventoModule {}
