0af9ba88cd96ec9351bbbc0ec5051fe8f5aa622a
Juan Alegría <j.alegria@uniandes.edu.co>
2023-03-13 09:29:54 -0500

    Feat: complete controller of event and review

--- src/evento/evento.controller.ts
+++ src/evento/evento.controller.ts
@@ -1,9 +1,41 @@
-import { Controller, UseInterceptors } from '@nestjs/common';
-import { BusinessErrorsInterceptor } from 'src/shared/interceptors/business-errors.interceptor';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
+import { EventoDto } from './evento.dto';
+import { EventoEntity } from './evento.entity';
 import { EventoService } from './evento.service';
 
 @Controller('eventos')
 @UseInterceptors(BusinessErrorsInterceptor)
 export class EventoController {
-  constructor(private readonly eventoService: EventoService) {}
+    constructor(private readonly eventoService: EventoService) {}
+
+  @Get()
+  async findAll() {
+    return await this.eventoService.findAll();
+  }
+
+  @Get(':eventoId')
+  async findOne(@Param('eventoId') eventoId: string) {
+    return await this.eventoService.findOne(eventoId);
+  }
+
+  @Post()
+  async create(@Body() eventoDto: EventoDto) {
+    const evento: EventoEntity = plainToInstance(EventoEntity, eventoDto);
+    return await this.eventoService.create(evento);
+  }
+
+  @Put(':eventoId')
+  async update(@Param('eventoId') eventoId: string, @Body() eventoDto: EventoDto) {
+    const evento: EventoEntity = plainToInstance(EventoEntity, eventoDto);
+    return await this.eventoService.update(eventoId, evento);
+  }
+
+  @Delete(':eventoId')
+  @HttpCode(204)
+  async delete(@Param('eventoId') eventoId: string) {
+    return await this.eventoService.delete(eventoId);
+  }
 }
--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,9 +1,41 @@
-import { Controller, UseInterceptors } from '@nestjs/common';
-import { BusinessErrorsInterceptor } from 'src/shared/interceptors/business-errors.interceptor';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
+import { ReviewDto } from './review.dto';
+import { ReviewEntity } from './review.entity';
 import { ReviewService } from './review.service';
 
 @Controller('reviews')
 @UseInterceptors(BusinessErrorsInterceptor)
 export class ReviewController {
-  constructor(private readonly reviewService: ReviewService) {}
+    constructor(private readonly reviewService: ReviewService) {}
+
+  @Get()
+  async findAll() {
+    return await this.reviewService.findAll();
+  }
+
+  @Get(':reviewId')
+  async findOne(@Param('reviewId') reviewId: string) {
+    return await this.reviewService.findOne(reviewId);
+  }
+
+  @Post()
+  async create(@Body() reviewDto: ReviewDto) {
+    const review: ReviewEntity = plainToInstance(ReviewEntity, reviewDto);
+    return await this.reviewService.create(review);
+  }
+
+  @Put(':reviewId')
+  async update(@Param('reviewId') reviewId: string, @Body() reviewDto: ReviewDto) {
+    const review: ReviewEntity = plainToInstance(ReviewEntity, reviewDto);
+    return await this.reviewService.update(reviewId, review);
+  }
+
+  @Delete(':reviewId')
+  @HttpCode(204)
+  async delete(@Param('reviewId') reviewId: string) {
+    return await this.reviewService.delete(reviewId);
+  }
 }
