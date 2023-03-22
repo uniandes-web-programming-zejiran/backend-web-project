dcc6bf8108ac299b38ee09e6d9ec46cb55b2c37f
Panis26 <ca.sanchezo1@uniandes.edu.co>
2023-03-22 12:27:26 -0500

    Auth y User

---
+++ src/user/user.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { User } from './user';
+
+@Injectable()
+export class UserService {
+    private users: User[] = [
+        new User(1, 'admin', 'admin', ['admin']),
+        new User(2, 'user', 'admin', ['user']),
+    ];
+
+    async findOne(username: string): Promise<User | undefined> {
+        return this.users.find(user => user.username === username);
+    }
+}
