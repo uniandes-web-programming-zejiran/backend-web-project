216dd680b1bf48e92ed0a646f65d9cc7aa62ed30
Juan Alegría <j.alegria@uniandes.edu.co>
2023-03-22 21:59:25 -0500

    Feat: add security to review-producto API

--- src/review-producto/review-producto.controller.ts
+++ src/review-producto/review-producto.controller.ts
@@ -8,6 +8,7 @@ import {
   Param,
   Post,
   Put,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { plainToInstance } from 'class-transformer';
@@ -15,12 +16,23 @@ import { ReviewDto } from 'src/review/review.dto';
 import { ReviewEntity } from 'src/review/review.entity';
 import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
 import { ReviewProductoService } from './review-producto.service';
+import { HasRoles } from '../auth/has-roles.decorator';
+import { Role } from '../auth/role.enum';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/roles.guard';
 
 @Controller('productos')
 @UseInterceptors(BusinessErrorsInterceptor)
 export class ReviewProductoController {
   constructor(private readonly reviewProductoService: ReviewProductoService) {}
 
+  @HasRoles(
+    Role.AdminReview,
+    Role.EscrituraReview,
+    Role.AdminProducto,
+    Role.EscrituraProducto,
+  )
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Post(':productoId/reviews/:reviewId')
   async addReviewProducto(
     @Param('productoId') productoId: string,
@@ -32,6 +44,13 @@ export class ReviewProductoController {
     );
   }
 
+  @HasRoles(
+    Role.AdminReview,
+    Role.LecturaReview,
+    Role.AdminProducto,
+    Role.LecturaProducto,
+  )
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Get(':productoId/reviews/:reviewId')
   async findReviewByProductoIdReviewId(
     @Param('productoId') productoId: string,
@@ -43,11 +62,25 @@ export class ReviewProductoController {
     );
   }
 
+  @HasRoles(
+    Role.AdminReview,
+    Role.LecturaReview,
+    Role.AdminProducto,
+    Role.LecturaProducto,
+  )
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Get(':productoId/reviews')
   async findReviewsByProductoId(@Param('productoId') productoId: string) {
     return await this.reviewProductoService.findReviewsByProductoId(productoId);
   }
 
+  @HasRoles(
+    Role.AdminReview,
+    Role.EscrituraReview,
+    Role.AdminProducto,
+    Role.EscrituraProducto,
+  )
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Put(':productoId/reviews')
   async associateReviewsProducto(
     @Body() reviewsDto: ReviewDto[],
@@ -60,6 +93,13 @@ export class ReviewProductoController {
     );
   }
 
+  @HasRoles(
+    Role.AdminReview,
+    Role.EliminarReview,
+    Role.AdminProducto,
+    Role.EliminarProducto,
+  )
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Delete(':productoId/reviews/:reviewId')
   @HttpCode(204)
   async deleteReviewProducto(
