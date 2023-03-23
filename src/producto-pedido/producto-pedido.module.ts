e40d332b2f9dbaf83bf75cbc3bdf7f25b069cb4b
amolinam08 <a.molinam@uniandes.edu.co>
2023-03-22 21:52:51 -0500

    Collections Final

--- src/producto-pedido/producto-pedido.module.ts
+++ src/producto-pedido/producto-pedido.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PedidoEntity } from 'src/pedido/pedido.entity';
 import { ProductoEntity } from 'src/producto/producto.entity';
 import { ProductoPedidoService } from './producto-pedido.service';
+import { ProductoPedidoController } from './producto-pedido.controller';
 
 @Module({
   providers: [ProductoPedidoService],
   imports: [TypeOrmModule.forFeature([PedidoEntity, ProductoEntity])],
+  controllers: [ProductoPedidoController],
 })
 export class ProductoPedidoModule {}
