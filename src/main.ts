4c2d7cbc4f083768e89cac510d09622cdbc97674
CamiloFallaM <c.falla@uniandes.edu.co>
2023-06-02 00:06:59 -0500

    CORS

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,8 @@ async function bootstrap() {
   });
 
   app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors();
 
   await app.listen(3000);
 }
